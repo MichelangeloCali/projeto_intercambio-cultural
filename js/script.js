8222a79fe594295d4556a5b0a52f7c3b02d131b2
Michelangelo Cali <michelangelocali@hotmail.com>
2022-05-10 15:27:03 -0300

    posição scroll animação

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 import ScrollSuave from "./modules/scrollSuave.js";
-import initTabNav from "./modules/tabNav.js";
 import initAccordion from "./modules/accordion.js";
-import initAnimaScroll from "./modules/animeScroll.js";
+import initTabNav from "./modules/tabNav.js";
 import initModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
 import initDropdownMenu from "./modules/menuDropdown.js";
@@ -9,19 +8,17 @@ import initMenuMobile from "./modules/menuMobile.js";
 import initOperation from "./modules/funcionamento.js";
 import initFetchNumbers from "./modules/animationApi.js";
 import initFetchBitcoin from "./modules/bitcoinApi.js";
+import initAnimaScroll from "./modules/animeScroll.js";
 
 // SCROLL SUAVE
 const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
 scrollSuave.init();
 
-// INICIO - NAVEGAÇÃO POR TAB
-initTabNav();
-
 // FAQ - ACCORDION
 initAccordion();
 
-// ANIMAÇÃO SCROLL
-initAnimaScroll();
+// INICIO - NAVEGAÇÃO POR TAB
+initTabNav();
 
 // Modal Login
 initModal();
@@ -43,3 +40,6 @@ initFetchNumbers();
 
 // API externa Bitcoin.
 initFetchBitcoin();
+
+// ANIMAÇÃO SCROLL
+initAnimaScroll();
